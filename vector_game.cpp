c407e40a2c490af7e7157d940eaf43033348d373
ktchow1 <ktchow1@yahoo.com.hk>
2021-04-26 00:13:26 +0800

    add string palindrome (with unsolved cases)

--- vector_game.cpp
+++ vector_game.cpp
@@ -118,9 +118,7 @@ auto count_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_
     std::unordered_map<T, int> hist; hist[0] = 1; // Don't forget this.
     std::uint32_t ans = 0;
 
-    // *** Extra variable (as compared to 2 point problem) *** //
     T sum = 0;
-
     for(auto iter=begin; iter!=end; ++iter)    
     {
         // *** Update sum *** //
@@ -152,9 +150,7 @@ auto count_target_divisible_subseq_sum(ITER begin, ITER end, const typename std:
     std::unordered_map<T, int> hist; hist[0] = 1; // Don't forget this.
     std::uint32_t ans = 0;
 
-    // *** Extra variable (as compared to 2 point problem) *** //
     T sum = 0;
-
     for(auto iter=begin; iter!=end; ++iter)    
     {
         // *** Update sum *** //
@@ -176,16 +172,19 @@ auto count_target_divisible_subseq_sum(ITER begin, ITER end, const typename std:
     return ans;
 }
 
+// ***************************************************** //
+// For this question and the next, 
+// replace std::unordered_map of count (i.e. histogram) 
+//    with std::unordered_map of index
+// ***************************************************** //
 template<typename ITER>
 auto longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
 {
     using T = typename std::iterator_traits<ITER>::value_type;
-    std::unordered_map<T, ITER> idx; idx[0] = begin; // index is the "end" of first subsequence having specific sum
+    std::unordered_map<T, ITER> idx; idx[0] = begin; // index is the "end" of first subseq having specific sum
     std::uint32_t ans = 0;
 
-    // *** Extra variable (as compared to 2 point problem) *** //
     T sum = 0;
-
     for(auto iter=begin; iter!=end; ++iter)    
     {
         // *** Update sum *** //
@@ -194,7 +193,7 @@ auto longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterato
         // *** Update ans *** //
         if (auto tmp = idx.find(sum - target); tmp!=idx.end())
         {
-            ans = std::max(std::distance(iter+1, tmp->second), ans);
+            ans = std::max(std::distance(tmp->second, iter+1), ans);
         }
 
         // *** Update idx *** //
@@ -206,6 +205,102 @@ auto longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterato
     return ans;
 }
 
+// ************************************************** //
+// 1. For positive number only
+// 2. For lower bound search of key instead of 
+//        exact match search of key, hence 
+//        use std::map instead of std::unordered_map.
+// ************************************************** //
+auto count_lt_target_subseq_sum_of_pos_numbers(std::vector<uint32_t>::iterator begin, 
+                                               std::vector<uint32_t>::iterator end,
+                                               std::uint32_t target)
+{
+    std::map<T, ITER> idx; idx[0] = begin; // index is the "end" of subseq starting with element 0 having specific sum
+    std::uint32_t ans = 0;
 
+    T sum = 0;
+    for(auto iter=begin; iter!=end; ++iter)    
+    {
+        // *** Update sum *** //
+        sum += *iter;
+
+        // *** Update ans *** //
+        if (auto tmp = idx.lower_bound(sum - target); tmp!=idx.end())
+        {
+            ans += std::distance(tmp->second, iter+1);
+        }
+
+        // *** Update idx *** //
+        if (auto tmp = idx.find(sum); tmp==idx.end())
+        {
+            idx[sum] = iter+1;
+        }
+    } 
+    return ans;
+}
+
+auto longest_non_duplicated_string(std::string::iterator begin,
+                                   std::string::iterator end)
+{
+    std::unordered_map<char, std::string::iterator> idx;
+    std::uint32_t sub = 0;
+    std::uint32_t ans = 0;
+
+    for(auto iter=begin; iter!=end; ++iter)
+    {
+        // *** Update sub & ans *** //
+        if (auto tmp = idx.find(*iter); tmp!=idx.end())
+        {
+            sub = std::distance(tmp->second, iter+1); 
+        }
+        else
+        {
+            sub = sub+1;
+        }
+        ans = std::max(ans, sub);
+
+        // *** Update idx *** //
+        if (auto tmp = idx.find(*iter); tmp==idx.end()) // This is duplicated for readability.
+        {
+            idx[*iter] = iter+1;
+        }
+    }
+    return ans;
+}
+
+// This method cannot solve cycle cases, like ABCBABCBA.
+auto longest_palindrome(std::string::iterator begin,
+                        std::string::iterator end)
+{
+    std::uint32_t sub0 = 1; //  odd palindrome : ABCBA
+    std::uint32_t sub1 = 0; // even palindrome : ABCCBA
+    std::uint32_t ans  = 1;
+
+    for(auto iter=begin+1; iter!=end; ++iter) // start with begin+1, avoid bound-checking in line A&B
+    {
+        // *** Update odd *** //
+        if (*iter == *(iter-sub0-1)) // line A
+        {
+            sub0 = sub0 + 2;
+        }
+        else
+        {
+            sub0 = 1;
+        }
+
+        // *** Update even *** //
+        if (*iter == *(iter-sub1-1)) // line B
+        {
+            sub1 = sub1 + 2;
+        }
+        else
+        {
+            sub1 = 0;
+        }
+
+        ans = std::max(ans, std::max(sub0, sub1));
+    }
+    return ans;
+}
 
 
