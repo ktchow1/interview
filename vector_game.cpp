ee0995eeb618e2c4de311b254d087400b2f2b23b
ktchow1 <ktchow1@yahoo.com.hk>
2021-04-27 01:36:23 +0800

    add vector game prototype

--- vector_game.cpp
+++ vector_game.cpp
@@ -2,31 +2,36 @@
 // *** This file is not tested. *** //
 // ******************************** //
 // Vector problem A
-// 1. max dist-sum 
+//  1. max dist-sum 
 //
 // Vector problem B
-// 2. max   profit                      : sub  / ans
-// 3. count target profit               : hist / ans
-// 4. max   subseq sum                  : sub  / ans
-// 5. max   subseq product              : sub  / ans
-// 6. max   non-contiguous subseq sum   : sub  / ans
-// 7. count target subseq sum           : sum  / hist / ans
-// 8. count target-divisible subseq sum : sum  / hist / ans
-// 9. count target-less-than subseq sum : sum  / hist / ans
-// 0. count longest target subseq sum   : sum  / hist / ans
+//  2. max   profit                      : sub  / ans
+//  3. count target profit               : hist / ans
+//  4. max   subseq sum                  : sub  / ans
+//  5. max   subseq product              : sub  / ans
+//  6. max   non-contiguous subseq sum   : sub  / ans
+//  7. count target subseq sum           : sum  / hist / ans
+//  8. count target-divisible subseq sum : sum  / hist / ans
+//  9. count target-less-than subseq sum : sum  / hist / ans
+// 10. count longest target subseq sum   : sum  / hist / ans
 //
-// String problem
-// 1. longest non-duplicated substr
-// 2. longest palindrome substr
-// 3. shortest unsorted substr
+// String problem & 2D problem
+// 11. longest non-duplicated substr
+// 12. longest palindrome substr
+// 13. set of subset
+// 14. nearest target pair-sum
+// 15. longest non-contiguous common subseq
 //
-// 2D problem
-// 4. set of subset
-// 5. nearest target pair-sum
-// 6. longest non-contiguous common subseq
+// Tree, sort-variant, stack-trick
+// 16. tree layer average
+// 17. order statistic   -  quick sort variant
+// 18. number of bribes  - bubble sort variant
+// 19. minimum unsorted part
+// 20. biggest rectangle
+// 21. biggest muddy puddle
 
 template<typename ITER>
-auto max_profit(ITER begin, ITER end)
+auto Q02_max_profit(ITER begin, ITER end)
 {
     auto sub = *(begin+1) - *begin;
     auto ans = sub;
@@ -40,7 +45,7 @@ auto max_profit(ITER begin, ITER end)
 }
 
 template<typename ITER>
-auto count_target_profit(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
+auto Q03_count_target_profit(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
 {
     using T = typename std::iterator_traits<ITER>::value_type;
     std::unordered_map<T, int> hist; hist[*begin] = 1;
@@ -68,7 +73,7 @@ auto count_target_profit(ITER begin, ITER end, const typename std::iterator_trai
 }
 
 template<typename ITER>
-auto max_subseq_sum(ITER begin, ITER end)
+auto Q04_max_subseq_sum(ITER begin, ITER end)
 {
     auto sub = *begin;
     auto ans = sub;
@@ -81,7 +86,7 @@ auto max_subseq_sum(ITER begin, ITER end)
 }
 
 template<typename ITER>
-auto max_subseq_product(ITER begin, ITER end)
+auto Q05_max_subseq_product(ITER begin, ITER end)
 {
     auto sub0 = *begin;
     auto sub1 = *begin;
@@ -96,7 +101,7 @@ auto max_subseq_product(ITER begin, ITER end)
 }
 
 template<typename ITER>
-auto max_non_contig_subseq_sum(ITER begin, ITER end)
+auto Q06_max_non_contig_subseq_sum(ITER begin, ITER end)
 {
     auto sub0 = *begin; // subproblem with constraint : must include element[n]
     auto sub1 = 0;      // subproblem with constraint : must exclude element[n]
@@ -112,7 +117,7 @@ auto max_non_contig_subseq_sum(ITER begin, ITER end)
 
 
 template<typename ITER>
-auto count_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
+auto Q07_count_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
 {
     using T = typename std::iterator_traits<ITER>::value_type;
     std::unordered_map<T, int> hist; hist[0] = 1; // Don't forget this.
@@ -144,7 +149,7 @@ auto count_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_
 }
 
 template<typename ITER>
-auto count_target_divisible_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
+auto Q08_count_target_divisible_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
 {
     using T = typename std::iterator_traits<ITER>::value_type;
     std::unordered_map<T, int> hist; hist[0] = 1; // Don't forget this.
@@ -178,7 +183,7 @@ auto count_target_divisible_subseq_sum(ITER begin, ITER end, const typename std:
 //    with std::unordered_map of index
 // ***************************************************** //
 template<typename ITER>
-auto longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
+auto Q09_longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterator_traits<ITER>::value_type& target)
 {
     using T = typename std::iterator_traits<ITER>::value_type;
     std::unordered_map<T, ITER> idx; idx[0] = begin; // index is the "end" of first subseq having specific sum
@@ -211,9 +216,9 @@ auto longest_target_subseq_sum(ITER begin, ITER end, const typename std::iterato
 //        exact match search of key, hence 
 //        use std::map instead of std::unordered_map.
 // ************************************************** //
-auto count_lt_target_subseq_sum_of_pos_numbers(std::vector<uint32_t>::iterator begin, 
-                                               std::vector<uint32_t>::iterator end,
-                                               std::uint32_t target)
+auto Q10_count_lt_target_subseq_sum_of_pos_numbers(std::vector<uint32_t>::iterator begin, 
+                                                   std::vector<uint32_t>::iterator end,
+                                                   std::uint32_t target)
 {
     std::map<T, ITER> idx; idx[0] = begin; // index is the "end" of subseq starting with element 0 having specific sum
     std::uint32_t ans = 0;
@@ -239,8 +244,8 @@ auto count_lt_target_subseq_sum_of_pos_numbers(std::vector<uint32_t>::iterator b
     return ans;
 }
 
-auto longest_non_duplicated_string(std::string::iterator begin,
-                                   std::string::iterator end)
+auto Q11_longest_non_duplicated_string(std::string::iterator begin,
+                                       std::string::iterator end)
 {
     std::unordered_map<char, std::string::iterator> idx;
     std::uint32_t sub = 0;
@@ -269,8 +274,8 @@ auto longest_non_duplicated_string(std::string::iterator begin,
 }
 
 // This method cannot solve cycle cases, like ABCBABCBA.
-auto longest_palindrome(std::string::iterator begin,
-                        std::string::iterator end)
+auto Q12_longest_palindrome(std::string::iterator begin,
+                            std::string::iterator end)
 {
     std::uint32_t sub0 = 1; //  odd palindrome : ABCBA
     std::uint32_t sub1 = 0; // even palindrome : ABCCBA
@@ -303,7 +308,50 @@ auto longest_palindrome(std::string::iterator begin,
     return ans;
 }
 
-void tree_layer_average(const node<T>* root, std::vector<std::pair<T, std::uint32_t>>& result)
+auto Q13_set_of_subset(const std::string& str, std::set<std::string>& output)
+{
+    if (str.size()==0) return;
+    
+    std::set<std::string> temp;
+    Q13_set_of_subset(str.substr(1), temp);
+    for(const auto& x:temp)
+    {
+        output.insert(x);
+        x.insert(0,str[0]);
+        output.insert(x);
+    }
+    output.insert(std::string(1. str[0]));
+}
+
+auto Q14_nearest_target_pair_sum(const std::vector<std::uint32_t>& vx, // suppose to be sorted
+                                 const std::vector<std::uint32_t>& vy, // suppose to be sorted
+                                 std::uint32_t target)
+{
+    std::uint32_t x = s0.size()-1; 
+    std::uint32_t y = 0;          
+    std::uint32_t win_x = x;
+    std::uint32_t win_y = y;
+    std::uint32_t min_diff = abs(vx[x] + vy[y] - target);
+    while(true)
+    {
+        auto sum = vx[x] + vy[x];
+
+        // step 1 : minimization
+        if (auto tmp = abs(sum - targe); tmp < min_diff)
+        {
+            min_diff = tmp;
+            win_x = x;
+            win_y = y; 
+        }
+
+        // step 2: netvigation
+        if (vx[x] + vy[y] < target) --x;
+        else ++y;
+    }
+    return std::make_pair(vx[win_x], vy[win_y]);
+}
+
+void Q16_tree_layer_average(const node<T>* root, std::vector<std::pair<T, std::uint32_t>>& result)
 {
     if (!root) return;
 
@@ -318,4 +366,41 @@ void tree_layer_average(const node<T>* root, std::vector<std::pair<T, std::uint3
         if (this_node->lhs) q.push(std::make_pair(layer+1, this_node->lhs));
         if (this_node->rhs) q.push(std::make_pair(layer+1, this_node->rhs));
     }
+}
+
+template<typename ITER>
+auto Q17_order_statistics(ITER begin, ITER end, std::uint32_t K)
+{
+    auto front = begin;
+    auto last = end-1;
+
+    while(front!=last)
+    {
+        // step 1 : quick-sort-iteration
+    
+    
+        // step 2 : bisection
+    }
+    return *front;
+}
+
+template<typename ITER>
+std::uint32_t Q18_num_of_bribes(ITER begin, ITER end)
+{
+
+}
+
+template<typename ITER>
+std::uint32_t Q19_num_of_sorted_element(ITER begin, ITER end)
+{
+}
+
+template<typename ITER>
+auto Q20_biggest_rectangle(ITER begin, ITER end)
+{
+}
+
+template<typename ITER>
+auto Q21_muddy_puddle(ITER begin, ITER end)
+{
 }
