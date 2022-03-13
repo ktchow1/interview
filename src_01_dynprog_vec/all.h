8be11612d1b5fc9ac8fe27d8b1f860b2b45859cb
ktchow1 <ktchow1@yahoo.com.hk>
2022-03-13 22:38:25 +0000

    reorganise files

---
+++ src_01_dynprog_vec/all.h
@@ -0,0 +1,44 @@
+#pragma once
+
+// *************************** //
+// *** Dynamic Programming *** //
+// *************************** //
+// 2|3 + 1|7 + 7|1
+// ------------------------------
+// 2.0.0 max subseq sum
+// 2.0.1 L shape block
+// 2.1.0 longest common subseq
+// 2.1.1 building bridge
+// 2.1.2 edit distance
+// 2.2.0 bell number
+// 2.3.0 boolean parenthesis
+// 2.3.1 two coin game
+// 2.3.2 two men city traversal 
+// 2.3.3 bitonic tour
+// 2.3.4 piecewise linear fitting
+// 2.3.5 box stacking
+// 2.3.6 bin packing
+// 2.4.0 viterbi 
+// 2.4.1 assembly line
+// 2.4.2 retrospective trader
+// 2.4.3 coin change
+// 2.4.4 knapsack problem
+// 2.4.5 job schedule
+// 2.4.6 equal partition
+// 2.5.0 Dijkstra
+// ------------------------------
+// Variables in state-time graph
+// 1. cost x
+// 2. reward y
+// 3. decision k
+// 4. time t
+// 5. objective sum(yx)
+// 6. constraint sum(kx) <= C
+// 7. recursive update equation 
+// ------------------------------
+
+namespace dynprog
+{
+     
+}
+
---
+++ src_02_dynprog/all.h
@@ -0,0 +1,48 @@
+#pragma once
+
+// ***************************************** //
+// *** Dynamic Programming - vector game *** //
+// ***************************************** //
+// 3|2 + 1|2 + 4|5 + 2|3 + 2|3 = 27 
+// --------------------------------------------
+// 1.0.0 max 2-pt sum-diff
+// 1.0.1 max 2-pt sum with equal digit-sum
+// 1.0.2 max 2-pt sum of non-attacking rooks
+// 1.1.0 count 2-pt sum in sorted vector
+// 1.1.1 count K-pt sum in unsorted vector
+// 1.2.0 max 2-pt profit
+// 1.3.0 count 2-pt target profit
+// 1.3.1 count 2-pt target absolute profit
+// 1.4.0 max subseq sum 
+// 1.4.1 max subseq product
+// 1.4.2 max non-contiguous seq sum 
+// 1.4.3 max puzzle score 
+// 1.5.0 count target subseq sum
+// 1.5.1 count divisible subseq sum
+// 1.5.2 length target subseq sum
+// 1.5.3 count less-than-target subseq sum
+// 1.5.4 count less-than-target subseq product
+// --------------------------------------------
+// 1.6.0 longest non-duplicated substr
+// 1.6.1 longest palindrome substr
+// 1.7.0 shortest unsorted subseq
+// 1.7.1 max rectangle in histogram
+// 1.7.2 max pool size
+// 1.8.0 min bribe
+// 1.8.1 K-th element
+// 1.9.0 subset
+// 1.9.1 tree layer average 
+// 1.9.2 string matching algorithm
+// --------------------------------------------
+// 
+//               max      count
+// --------------------------------------------
+// 2pt_diff      sub      hist
+// subseq_sum    sub      hist + cum
+// --------------------------------------------
+
+namespace dynprog_vec
+{
+     
+}
+
